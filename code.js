a69e149b7b478295cbd8f59f83ed2ecb3f36637d
Quincy Morris Reynolds <143751384+qdog307@users.noreply.github.com>
2024-11-30 17:53:57 -0700

    Updated

--- code.js
+++ code.js
@@ -1,8 +1,28 @@
 function flip(array, n) {
+    let left = 0;
+    let right = n - 1;
+    while (left < right) {
+        [array[left], array[right]] = [array[right], array[left]];
+        left++;
+        right--;
+    }
+}
+
+function pancakeSort(array) {
+    const n = array.length;
+    for (let size = n; size > 1; size--) {
+        let maxIndex = 0;
+        for (let i = 1; i < size; i++) {
+            if (array[i] > array[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        if (maxIndex === size - 1) continue;
+        if (maxIndex > 0) {
+            flip(array, maxIndex + 1);
+        }
+        flip(array, size);
+    }
     return array;
 }
 
-// Use only flip() here to manipulate the array
-function pancakeSort(array) {
-    return array;
-}
